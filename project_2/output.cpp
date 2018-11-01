7db49b8078ee4024e5e0397828f901d221ce7fd3
Alec Foster <sirslush@email.arizona.edu>
2018-11-01 13:07:26 -0700

    message goes here

--- project_2/output.cpp
+++ project_2/output.cpp
@@ -13,7 +13,7 @@
 int outputFunct(list<variable> varList, list<conections> computations){
     ofstream myoutfile;
     
-    myoutfile.open ("Ciruit1.txt");
+    myoutfile.open ("outputCiruit.txt");
     myoutfile << "`timescale 1ns / 1ps\n\n\n";
     
     myoutfile << "module circuit1(";
@@ -21,7 +21,12 @@ int outputFunct(list<variable> varList, list<conections> computations){
 //        std::cout << it->name;
 //    }
     for (list<variable>::iterator it = varList.begin(); it != varList.end(); ++it) {
+        if (it->getVarnames() == "xwire" || it->getVarnames() == "ywire") {
+            
+        }
+        else{
         myoutfile << it->getVarnames() << " ,";
+        }
     }
     myoutfile << ");\n";
     
@@ -34,13 +39,62 @@ int outputFunct(list<variable> varList, list<conections> computations){
     myoutfile << it->getSize() << "-1:0] ";
     myoutfile << it->getVarnames() << ";\n";
     myoutfile << "\tinput Clk, Rst;\n";
-    myoutfile << "\toutput [Int(size)-1:0] (output);\n";
+    //outputs
+    it++;
+    
+    myoutfile << "\toutput [Int";
+    myoutfile << it->getSize() << "-1:0] ";
+    myoutfile << it->getVarnames() << ";\n";
     myoutfile << "\toutput [Int(size)-1:0] (output);\n";
     
     myoutfile << "\twire (wirename);\n";
     myoutfile << "\twire [Int(size)-1:0] (wire);\n";
     myoutfile << "\twire [Int(size)-1:0] (wire);\n";
     
+    myoutfile << "\n\n";
+    // make modules
+    for (list<conections>::iterator it = computations.begin(); it != computations.end(); ++it) { myoutfile << "\t";
+        if (it->getOperation() == "=+") {
+            myoutfile << "make addition with (" << it->getInputs() << ")\n";
+            myoutfile << "\t\tand output " << it->getName() << "\n";
+        }
+        else if (it->getOperation() == "=-"){
+            myoutfile << "make subtraction with (" << it->getInputs() << ")\n";
+            myoutfile << "\t\tand output " << it->getName() << "\n";
+        }
+        else if (it->getOperation() == "==="){
+            myoutfile << "make comparitor with (" << it->getInputs() << ")\n";
+            myoutfile << "\t\tand output " << it->getName() << "\n";
+        }
+        else if (it->getOperation() == "=<"){
+            myoutfile << "make comparitor with (" << it->getInputs() << ")\n";
+            myoutfile << "\t\tand output " << it->getName() << "\n";
+        }
+        else if (it->getOperation() == "=>"){
+            myoutfile << "make comparitor with (" << it->getInputs() << ")\n";
+            myoutfile << "\t\tand output " << it->getName() << "\n";
+        }
+        else if (it->getOperation() == "=<<"){
+            myoutfile << "make shifter with (" << it->getInputs() << ")\n";
+            myoutfile << "\t\tand output " << it->getName() << "\n";
+        }
+        else if (it->getOperation() == "=>>"){
+            myoutfile << "make comparitor with (" << it->getInputs() << ")\n";
+            myoutfile << "\t\tand output " << it->getName() << "\n";
+        }
+        else if (it->getOperation() == "=?"){
+            myoutfile << "make comparitor with (" << it->getInputs() << ")\n";
+            myoutfile << "\t\tand output " << it->getName() << "\n";
+        }
+        else if (it->getOperation() == "=*"){
+            myoutfile << "make multiplier with (" << it->getInputs() << ")\n";
+            myoutfile << "\t\tand output " << it->getName() << "\n";
+        }
+        else if (it->getOperation() == "="){
+            myoutfile << "make wireconection with (" << it->getInputs() << ")\n";
+            myoutfile << "\t\tand output " << it->getName() << "\n";
+        }
+    }
     
     
     myoutfile << "\n\nendmodule";
